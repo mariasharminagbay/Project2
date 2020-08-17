c65262a2f293a2c1f08b8945ce21b6d817e678a0
fpatel3 <forampatel3010@gmail.com>
2020-08-17 15:24:36 -0400

    Update portfolio.js

--- iedirisi/portfolio.js
+++ iedirisi/portfolio.js
@@ -1,14 +1,24 @@
-function download() { 
+// function download() { 
               
-    // var text = document.getElementById("text").value; 
-    var file = "/iedirisi/Resume_Ishara-Jobs.pdf"; 
-    document.getElementById('my_iframe').src = file;
-} 
+//     // var text = document.getElementById("text").value; 
+//     var file = "/iedirisi/Resume_Ishara-Jobs.pdf"; 
+//     document.getElementById('my_iframe').src = file;
+// } 
   
 // console.log(document.getElementById("btnResume"))
 // document.getElementById("btnResume") 
 // .addEventListener("click", function() { 
-    
+
+    function download() {
+        var fileURL = "/iedirisi/Resume_Ishara-Jobs.pdf"; 
+        var fileName = "Resume_Ishara-Jobs.pdf";
+        var link = document.createElement('a');
+        link.href = fileURL;
+        link.download = fileName;
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+      }
    
   
 //     download(filename, text); 
