9cae9a4aaa7f10561ac7b56fcdbfddc7a88e89e8
mariasharmagbay <mariasharmagbay@gmail.com>
2020-08-17 10:58:33 -0400

    getting update

--- SCRIPTS/gallery.js
+++ SCRIPTS/gallery.js
@@ -68,3 +68,22 @@ for (var i = 0; i < btns.length; i++) {
     this.className += " active";
   });
 }
+
+function zoomin(){
+  var myImg = document.getElementById("sky");
+  var currWidth = myImg.clientWidth;
+  if(currWidth == 500){
+      alert("Maximum zoom-in level reached.");
+  } else{
+      myImg.style.width = (currWidth + 50) + "px";
+  } 
+}
+function zoomout(){
+  var myImg = document.getElementById("sky");
+  var currWidth = myImg.clientWidth;
+  if(currWidth == 50){
+      alert("Maximum zoom-out level reached.");
+  } else{
+      myImg.style.width = (currWidth - 50) + "px";
+  }
+}
